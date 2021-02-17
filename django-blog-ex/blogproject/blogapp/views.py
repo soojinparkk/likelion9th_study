0d8063539803168eebb77e0f96e2cedef9851160
sooooz <soojinpar1026@gmail.com>
2021-02-17 20:33:36 +0900

    add blogproject-CREATE

--- django-blog-ex/blogproject/blogapp/views.py
+++ django-blog-ex/blogproject/blogapp/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import Blog
+from django.utils import timezone
 
 def home(request):
     blogs = Blog.objects.all()
@@ -9,3 +10,15 @@ def detail(request, id):
     # blog = Blog.objects.get(id = id)
     blog = get_object_or_404(Blog, pk = id)
     return render(request, 'detail.html', {'blog': blog})
+
+def new(request):
+    return render(request, 'new.html')
+
+def create(request):
+    new_blog = Blog()                                   # 새로운 객체에 저장
+    new_blog.title = request.POST['title']
+    new_blog.write = request.POST['writer']
+    new_blog.body = request.POST['body']
+    new_blog.pub_date = timezone.now()                  # 현재 시각 저장
+    new_blog.save()
+    return redirect('detail', new_blog.id)              # detail 호출
