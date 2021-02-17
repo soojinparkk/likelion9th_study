5930b8291e403dc970385c888e56b9e765b243e2
sooooz <soojinpar1026@gmail.com>
2021-02-17 22:10:59 +0900

    add blogproject-UPDATE

--- django-blog-ex/blogproject/blogapp/views.py
+++ django-blog-ex/blogproject/blogapp/views.py
@@ -20,5 +20,18 @@ def create(request):
     new_blog.write = request.POST['writer']
     new_blog.body = request.POST['body']
     new_blog.pub_date = timezone.now()                  # 현재 시각 저장
-    new_blog.save()
+    new_blog.save()                                     # DB에 적용
     return redirect('detail', new_blog.id)              # detail 호출
+
+def edit(request, id):
+    edit_blog = Blog.objects.get(id = id)
+    return render(request, 'edit.html', {'blog': edit_blog})
+
+def update(request, id):
+    update_blog = Blog.objects.get(id = id)
+    update_blog.title = request.POST['title']
+    update_blog.writer = request.POST['writer']
+    update_blog.body = request.POST['body']
+    update_blog.pub_date = timezone.now()
+    update_blog.save()
+    return redirect('detail', update_blog.id)
