bdc7024624ad297b3fbba4355123b29c3311db8f
sooooz <soojinpar1026@gmail.com>
2021-02-23 22:49:35 +0900

    modify templates

--- django-blog-ex/blogproject/lionproject/settings.py
+++ django-blog-ex/blogproject/lionproject/settings.py
@@ -50,7 +50,7 @@ ROOT_URLCONF = 'lionproject.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['lionproject/templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
