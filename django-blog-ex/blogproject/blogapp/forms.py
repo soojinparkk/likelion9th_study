f1faf6fa460f34a429684511702ad0664a1e9695
sooziini <soojinpar1026@gmail.com>
2021-03-28 22:05:53 +0900

    modify forms.py and use bootstrap

--- django-blog-ex/blogproject/blogapp/forms.py
+++ django-blog-ex/blogproject/blogapp/forms.py
@@ -2,6 +2,33 @@ from django import forms
 from .models import Blog
 
 class BlogForm(forms.ModelForm):
+    # ModelForm customizing
+    title = forms.CharField(
+        label="제목:",
+        widget=forms.TextInput(attrs={
+            "placeholder": "제목을 입력해주세요",
+        })
+    )
+    writer = forms.CharField(
+        label="작성자:",
+        widget=forms.TextInput(attrs={
+            "placeholder": "작성자를 입력해주세요",
+        })
+    )
+    body = forms.CharField(
+        label="내용:",
+        widget=forms.Textarea(attrs={
+            "placeholder": "내용을 입력해주세요",
+            'cols': 50, 'rows': 13
+        })
+    )
+    image = forms.ImageField(
+        label="",
+        widget=forms.FileInput(attrs={
+            "placeholder": "작성자를 입력해주세요"
+        })
+    )
+
     class Meta:         # Blog 클래스에 대한 이름표 역할
         model = Blog
         fields = ['title', 'writer', 'body', 'image']
